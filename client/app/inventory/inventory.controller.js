40d3216660ca0565dfb408c3036f02da6c4c0015
Janos Brauer <j.brauer@sap.com>
2016-04-02 12:50:30 -0700

    Adds description of field

--- client/app/inventory/inventory.controller.js
+++ client/app/inventory/inventory.controller.js
@@ -4,4 +4,7 @@ angular.module('kickappApp')
   .controller('InventoryCtrl', function($scope, $stateParams) {
     var iId = $stateParams.fieldId;
     console.log(iId);
+    $scope.getSafetyness = function(selectedField) {
+      return new Array(3);
+    }
   });
