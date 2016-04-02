0a70e822de386c8360e78eb47f61c9a8d905c465
Janos Brauer <j.brauer@sap.com>
2016-04-02 13:08:53 -0700

    Adds back button

--- client/app/inventory/inventory.controller.js
+++ client/app/inventory/inventory.controller.js
@@ -1,10 +1,13 @@
 'use strict';
 
 angular.module('kickappApp')
-  .controller('InventoryCtrl', function($scope, $stateParams) {
+  .controller('InventoryCtrl', function($scope, $stateParams, $window) {
     var iId = $stateParams.fieldId;
     console.log(iId);
     $scope.getSafetyness = function(selectedField) {
       return new Array(3);
-    }
+    };
+    $scope.onBackButtonClick = function() {
+      $window.history.back();
+    };
   });
